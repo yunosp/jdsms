51c44a9f42524e917d107c735d0084aab8245892
jdzackxin <91025315+jdzackxin@users.noreply.github.com>
2021-09-21 11:03:54 +0800

    Add files via upload

--- api/sms.php
+++ api/sms.php
@@ -6,8 +6,6 @@ foreach($_REQUEST as $key => $value) {
 	$ui[$key] = trim($value);
 }
 $postData = file_get_contents("php://input");
-$UA="Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
-
 if(!empty($postData)) {
 	$data = json_decode($postData);
 	// var_dump($data);
